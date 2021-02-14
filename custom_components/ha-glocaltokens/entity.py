40400764d52813f3f4b794a511ef2942562d6ee5
DurgNomis-drol <67142049+DurgNomis-drol@users.noreply.github.com>
2021-02-14 10:53:09 +0100

    Merge new component - Initial commit  (#11)
    * Add files via upload
    
    * Update api.py
    
    * Update const.py
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- custom_components/ha-glocaltokens/entity.py
+++ custom_components/ha-glocaltokens/entity.py
@@ -4,7 +4,9 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
 from .const import MANUFACTURER
 from .const import NAME
+from .const import DEFAULT_NAME
 from .const import VERSION
+from .const import ICON_TOKEN, ICON_ALARMS, ICON_TIMERS
 
 
 class GlocaltokensEntity(CoordinatorEntity):
@@ -13,23 +15,104 @@ class GlocaltokensEntity(CoordinatorEntity):
         self.config_entry = config_entry
 
     @property
-    def unique_id(self):
-        """Return a unique ID to use for this entity."""
-        return self._name
-
-    @property
     def device_info(self):
         return {
-            "identifiers": {(DOMAIN, self.unique_id)},
-            "name": NAME,
+            "identifiers": {(DOMAIN, self._name)},
+            "name": f"{DEFAULT_NAME} {self._name}",
             "model": VERSION,
             "manufacturer": MANUFACTURER,
         }
 
     @property
-    def device_state_attributes(self):
-        """Return the state attributes."""
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{DEFAULT_NAME} {self._name} token"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        return f"{self._name}/token"
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return ICON_TOKEN
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def device_class(self):
+        """Return de device class of the sensor."""
+        return "glocaltokens__custom_device_class"
+
+class GlocalAlarmEntity(CoordinatorEntity):
+    def __init__(self, coordinator, config_entry):
+        super().__init__(coordinator)
+        self.config_entry = config_entry
+
+    @property
+    def device_info(self):
         return {
-            "id": str(self.unique_id),
-            "integration": DOMAIN,
+            "identifiers": {(DOMAIN, self._name)},
+            "name": f"{DEFAULT_NAME} {self._name}",
+            "model": VERSION,
+            "manufacturer": MANUFACTURER,
         }
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{DEFAULT_NAME} {self._name} next alarm"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        return f"{self._name}/next_alarm"
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return ICON_ALARMS
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+class GlocalTimersEntity(CoordinatorEntity):
+    def __init__(self, coordinator, config_entry):
+        super().__init__(coordinator)
+        self.config_entry = config_entry
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{DEFAULT_NAME} {self._name} timers"
+
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._name)},
+            "name": f"{DEFAULT_NAME} {self._name}",
+            "model": VERSION,
+            "manufacturer": MANUFACTURER,
+        }
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        return f"{self._name}/timers"
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return ICON_TIMERS
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
