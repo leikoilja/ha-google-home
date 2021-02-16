c7abd9ee9c6b167e2d1cb4653060172eb879d8bf
Ilja Leiko <leikoilja@gmail.com>
2021-02-16 14:34:47 +0100

    Bumping up glocaltokens version to v2

--- custom_components/ha-glocaltokens/const.py
+++ custom_components/ha-glocaltokens/const.py
@@ -1,10 +1,10 @@
 """Constants for Google local authentication token fetching."""
 # Base component constants
 NAME = "Google local authentication token fetching (Glocaltokens)"
-DOMAIN = "glocaltokens"
+DOMAIN = "ha-glocaltokens"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
-MANUFACTURER = "Glocaltokens"
+MANUFACTURER = "ha-glocaltokens"
 
 ATTRIBUTION = "json"
 ISSUE_URL = "https://github.com/leikoilja/ha-glocaltokens/issues"
@@ -29,38 +29,38 @@ CONF_PASSWORD = "password"
 CONF_MASTER_TOKEN = "master_token"
 
 # Defaults
-DEFAULT_NAME = "Glocaltokens"
+DEFAULT_NAME = "HA-GlocalTokens"
 
 # API
 PORT = 8443
-API_ENDPOINT_ALARMS = 'setup/assistant/alarms'
-HEADER_CAST_LOCAL_AUTH = 'cast-local-authorization-token'
+API_ENDPOINT_ALARMS = "setup/assistant/alarms"
+HEADER_CAST_LOCAL_AUTH = "cast-local-authorization-token"
 HEADER_CONTENT_TYPE = "content-type"
 
 HEADERS = {
-    HEADER_CAST_LOCAL_AUTH: '',
-    HEADER_CONTENT_TYPE: 'application/json; charset=UTF-8',
+    HEADER_CAST_LOCAL_AUTH: "",
+    HEADER_CONTENT_TYPE: "application/json; charset=UTF-8",
 }
 
 # TIMERS & ALARMS ATTRIBUTES
-FIRE_TIME = 'fire_time'
-FIRE_TIME_IN_S = 'fire_time_in_s'
-DATE_TIME = 'date_time'
-LOCAL_TIME = 'local_time'
-DURATION = 'duration'
+FIRE_TIME = "fire_time"
+FIRE_TIME_IN_S = "fire_time_in_s"
+DATE_TIME = "date_time"
+LOCAL_TIME = "local_time"
+DURATION = "duration"
 ORIGINAL_DURATION = "original_duration"
 
 # TIMESTRINGS
-SHOW_TIME_ONLY = '%H:%M:%S'
-SHOW_DATE_TIMEZONE = '%Y-%m-%dT%H:%M:%S.%fZ%Z'
-SHOW_DATE_AND_TIME = '%Y-%m-%d %H:%M:%S'
+SHOW_TIME_ONLY = "%H:%M:%S"
+SHOW_DATE_TIMEZONE = "%Y-%m-%dT%H:%M:%S.%fZ%Z"
+SHOW_DATE_AND_TIME = "%Y-%m-%d %H:%M:%S"
 
 # API Error
-API_RETURNED_UNKNOWN = 'API returned unknown json structure'
+API_RETURNED_UNKNOWN = "API returned unknown json structure"
 
 # Access token only lives about 1 hour
 # Update often to fetch timers in timely manner
-SCAN_INTERVAL = 15 # Seconds
+SCAN_INTERVAL = 15  # Seconds
 
 DEVICE_NAME = "deviceName"
 DEVICE_IP = "ip"
