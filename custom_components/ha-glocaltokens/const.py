5aa796b08a5b770e1ca29eaffc086a328812154a
DurgNomis-drol <67142049+DurgNomis-drol@users.noreply.github.com>
2021-02-17 10:13:20 +0100

    Updated const to match glocaltokens json output

--- custom_components/ha-glocaltokens/const.py
+++ custom_components/ha-glocaltokens/const.py
@@ -62,10 +62,10 @@ API_RETURNED_UNKNOWN = "API returned unknown json structure"
 # Update often to fetch timers in timely manner
 SCAN_INTERVAL = 15  # Seconds
 
-DEVICE_NAME = "deviceName"
+DEVICE_NAME = "device_name"
 DEVICE_IP = "ip"
 DEVICE_PORT = "port"
-TOKEN = "localAuthToken"
+TOKEN = "local_auth_token"
 ALARMS = "alarm"
 TIMERS = "timer"
 
