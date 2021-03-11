3b86ae42e6ffd0928ed48bf5ffd9aca338e611ee
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2021-03-11 17:08:58 +0000

    Fix unused-argument warning (#87)

--- custom_components/google_home/__init__.py
+++ custom_components/google_home/__init__.py
@@ -30,7 +30,7 @@ from .const import (
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
-async def async_setup(hass: HomeAssistant, config: Config):
+async def async_setup(_hass: HomeAssistant, _config: Config):
     """Set up this integration using YAML is not supported."""
     return True
 
