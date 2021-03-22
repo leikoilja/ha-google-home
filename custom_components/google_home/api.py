65da3520542dda103d61557b299462d4c72732ed
DurgNomis-drol <simongrud@gmail.com>
2021-03-22 18:20:09 +0100

    Changed exc to ex

--- custom_components/google_home/api.py
+++ custom_components/google_home/api.py
@@ -151,13 +151,13 @@ class GlocaltokensApiClient:
                         response.status,
                         response,
                     )
-        except Exception as exc:  # pylint: disable=broad-except
+        except Exception as ex:  # pylint: disable=broad-except
             # Make sure that we log the exception if one occurred.
             # The only reason we do this broad is so we easily can
             # debug the application.
             _LOGGER.error(
                 "Request error: %s",
-                exc,
+                ex,
             )
         finally:
             if resp:
