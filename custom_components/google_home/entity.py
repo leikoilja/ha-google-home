fbb3309e142afb365314ce4b748df2237b434008
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2021-03-23 19:27:11 +0100

    Add device class for next alarm and next timer. (#107)

--- custom_components/google_home/entity.py
+++ custom_components/google_home/entity.py
@@ -2,6 +2,7 @@
 
 from typing import Set, Tuple, TypedDict
 
+from homeassistant.const import DEVICE_CLASS_TIMESTAMP
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
@@ -65,7 +66,7 @@ class GoogleHomeDeviceEntity(CoordinatorEntity):
 
     @property
     def device_class(self) -> str:
-        """Return de device class of the sensor."""
+        """Return the device class of the sensor."""
         return "google_home__custom_device_class"
 
 
@@ -132,6 +133,11 @@ class GoogleHomeNextAlarmEntity(CoordinatorEntity):
         """Icon to use in the frontend, if any."""
         return ICON_ALARMS
 
+    @property
+    def device_class(self) -> str:
+        """Return the device class of the sensor."""
+        return DEVICE_CLASS_TIMESTAMP
+
 
 class GoogleHomeTimersEntity(CoordinatorEntity):
     """Entity base for timers sensor"""
@@ -195,3 +201,8 @@ class GoogleHomeNextTimerEntity(CoordinatorEntity):
     def icon(self) -> str:
         """Icon to use in the frontend, if any."""
         return ICON_TIMERS
+
+    @property
+    def device_class(self) -> str:
+        """Return the device class of the sensor."""
+        return DEVICE_CLASS_TIMESTAMP
